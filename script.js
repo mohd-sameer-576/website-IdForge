fd0c68af8f67d402c54adaa4fa7c81315b2d7a68
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2025-05-29 19:52:02 +0530

    some changes

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
+
 // let nbtn = document.getElementById('normal-btn');
-// nbtn.addEventListener('click', function() {
-//     this.classList.toggle('active');
-// });
 // let qrbtn = document.getElementById('qr-btn');
-// qrbtn.addEventListener('click', function() {
-//     this.classList.toggle('active');
+// let output = document.getElementsByClassName('welcome');
+// let nofo = document.getElementsByClassName('normalForm');
+// let qrfo = document.getElementsByClassName('qrForm');
+
+// nbtn.addEventListener('click', function() {
+//     if (!nbtn.classList.contains('active')) {
+//         nbtn.classList.add('active');
+//         qrbtn.classList.remove('active');
+//         // nofo[0].style.display = 'flex';
+//         // qrfo[0].style.display = 'none';
+//         output[0].innerHTML = '';
+//         output[0].appendChild(nofo[0].cloneNode(true)); // Add normal form
+//     } else {
+//         nbtn.classList.remove('active');
+//         // nofo[0].style.display = 'none';
+//         output[0].innerHTML = '';
+
+//     }
+//     // output[0].innerHTML = "Welcome to the Normal Mode!";
 // });
+
+// qrbtn.addEventListener('click', function() {
+//     if (!qrbtn.classList.contains('active')) {
+//         qrbtn.classList.add('active');
+//         nbtn.classList.remove('active');
+//         qrfo[0].style.display = 'flex';
+//         nofo[0].style.display = 'none';
+//         output[0].innerHTML = '';
+//     } else {
+//         qrbtn.classList.remove('active');
+//         qrfo[0].style.display = 'none';
+//         output[0].innerHTML = '';
+//     }
+//     // output[0].innerHTML = "Welcome to the QR Code Mode!";
+// });
+
 let nbtn = document.getElementById('normal-btn');
 let qrbtn = document.getElementById('qr-btn');
+let output = document.getElementsByClassName('welcome')[0];
+let nofo = document.getElementsByClassName('normalForm')[0];
+let qrfo = document.getElementsByClassName('qrForm')[0];
 
 nbtn.addEventListener('click', function() {
     if (!nbtn.classList.contains('active')) {
         nbtn.classList.add('active');
         qrbtn.classList.remove('active');
+
+        output.innerHTML = ''; // clear old form
+        output.appendChild(nofo.cloneNode(true)); // add Normal Form clone
     } else {
         nbtn.classList.remove('active');
+        output.innerHTML = ''; // remove form
     }
 });
 
@@ -22,7 +60,11 @@ qrbtn.addEventListener('click', function() {
     if (!qrbtn.classList.contains('active')) {
         qrbtn.classList.add('active');
         nbtn.classList.remove('active');
+
+        output.innerHTML = ''; // clear old form
+        output.appendChild(qrfo.cloneNode(true)); // add QR Form clone
     } else {
         qrbtn.classList.remove('active');
+        output.innerHTML = ''; // remove form
     }
 });
